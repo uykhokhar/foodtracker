10d799fbda5f6c50fb51cc2fb1de54ec6e3ed4ec
Umar Khokhar <ukhokhar@uchicago.edu>
2017-01-16 15:33:53 -0600

    FoodTracker Basic UI

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by MouseHouseApp on 1/16/17.
+//  Copyright © 2017 Umar Khokhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+
+        // Hide the keyboard
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+    
+        mealNameLabel.text = textField.text
+    
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+        mealNameLabel.text = "Default Text"
+    
+    }
+
+
+}
+
